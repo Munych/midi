648c2b50ebc635823f9fccae705e294ba465790a
Munych <dimkamunov@gmail.com>
2023-03-28 01:27:19 +0300

    Parse chunks

--- Midi.js
+++ Midi.js
@@ -5,6 +5,7 @@ class Midi {
     constructor(fileName) {
         this.fileName = fileName;
         this.offset = 0;
+        this.tracks = [];
 
         this.extractFileData();
     }
@@ -15,21 +16,78 @@ class Midi {
     }
 
     parse() {
-        let chunkType = Utils.getBytes(this.offset, 4, this.rawData);
-        console.log(Utils.hexToAscii(chunkType));
+        const chunkHeaderType = Utils.getBytes(this.offset, 4, this.rawData);
+        this.changeOffset(4);
 
-        // this.midiHeaderTrack = new MidiHeaderTrack(this.getBytes(2));
+        const chunkHeaderLength = Utils.getBytes(this.offset, 4, this.rawData);
+        this.changeOffset(4);
+        const chunkHeaderFormat = Utils.getBytes(this.offset, 2, this.rawData);
+        this.changeOffset(2);
+        const chunkHeaderNumberTracks = Utils.getBytes(this.offset, 2, this.rawData);
+        this.changeOffset(2);
+        const chunkHeaderDivision = Utils.getBytes(this.offset, 2, this.rawData);
+        this.changeOffset(2);
+        
+        this.midiHeaderTrack = new MidiHeaderTrack(chunkHeaderType, chunkHeaderLength, chunkHeaderFormat, chunkHeaderNumberTracks, chunkHeaderDivision);
+        // this.midiHeaderTrack.print();
+
+        for(let i = 0; i < this.midiHeaderTrack.chunkNumberTracks; i++) {
+            const chunkTypeTrack = Utils.getBytes(this.offset, 4, this.rawData);
+            this.changeOffset(4);
+            const chunkLengthTrack = Utils.getBytes(this.offset, 4, this.rawData);
+            this.changeOffset(4);
+            const chunkEvents = Utils.getBytes(this.offset, parseInt(chunkLengthTrack, 16), this.rawData);
+            this.changeOffset(parseInt(chunkLengthTrack, 16));
+            
+            const midiTrack = new MidiTrack(chunkTypeTrack, chunkLengthTrack, chunkEvents);
+            // midiTrack.print();
+
+            this.tracks.push(midiTrack);
+        }
+
+        console.log(this);
+    }
+
+    changeOffset(offset) {
+        this.offset += offset * 2;
     }
 }
 
 class MidiHeaderTrack {
-    constructor(binaryData) {
-        this.binaryData = binaryData;
-        this.parse();
+    constructor(chunkType, chunkLength, chunkFormat, chunkNumberTracks, division) {
+        this.chunkType = Utils.hexToAscii(chunkType);
+        this.chunkLength = parseInt(chunkLength, 16);
+        this.chunkFormat = parseInt(chunkFormat, 16);
+        this.chunkNumberTracks = parseInt(chunkNumberTracks, 16);
+        this.division = division;
     }
 
     parse() {
         console.log(this.binaryData);
+    }
+
+    print() {
+        console.log('chunkType', this.chunkType);
+        console.log('chunkLength', this.chunkLength);
+        console.log('chunkFormat', this.chunkFormat);
+        console.log('chunkNumberTracks', this.chunkNumberTracks);
+        console.log('division', this.division);
+    }
+}
+
+class MidiTrack {
+    constructor(chunkType, chunkLength, chunkEvents) {
+        this.chunkType = Utils.hexToAscii(chunkType);
+        this.chunkLength = parseInt(chunkLength, 16);
+        this.chunkEvents = chunkEvents;
+
+        // this.print();
+    }
+
+    print() {
+        console.log('chunkType', this.chunkType);
+        console.log('chunkLength', this.chunkLength);
+        console.log('chunkEvents', this.chunkEvents);
     }
 }
 
